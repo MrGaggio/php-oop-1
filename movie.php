0e95be51f0b5de8e7278d3802594974c28210d3e
Stefano Ruggiu <steruggiu@gmail.com>
2022-02-15 17:14:30 +0000

    inizializzate le variabili

--- movie.php
+++ movie.php
@@ -3,24 +3,49 @@
 //crea classe Movies
 class Movies
 {
-    public $tile;
+    public $title;
+    public $genre;
     public $year;
-    public $actors;
+    public $characters;
     public $musics;
     public $movie_director;
-    public $photography;
     public $imdb_rate;
     public $film_production_house;
-    
+
     //crea un costruttore
-    public function __construct($title, $movie_director, $film_production_house)
-{
-    $this->title = $title;
-    $this->movie_director = $movie_director;
-    $this->film_production_house = $film_production_house;
-}
+//     public function __construct($title, $movie_director, $film_production_house)
+// {
+//     $this->title = $title;
+//     $this->movie_director = $movie_director;
+//     $this->film_production_house = $film_production_house;
+// }
 }
 
 //crea 2 variabili d'istanza
 $films = new Movies();
 $cartoons = new Movies();
+
+$films->title = 'Matrix';
+$films->genre = 'scienze fiction';
+$films->year = '1999';
+$films->characters = 'Neo, Morpheus, Trinity';
+$films->musics = 'Wake Up - Rage Against the Machine';
+$films->movie_director = 'Lana Wachowski, Lilly Wachowski';
+$films->imdb_rate = '8.7';
+$films->film_production_house = 'Warner Bros. Pictures';
+
+$cartoons->title = 'Wall-e';
+$cartoons->genre = 'scienze fiction';
+$cartoons->year = '2008';
+$cartoons->characters = 'Wall-e';
+$cartoons->musics = 'Michael Crawford – Put On Your Sunday Clothes';
+$cartoons->movie_director = 'Disney Pixar';
+$cartoons->imdb_rate = '8.4';
+$cartoons->film_production_house = 'Warner Bros. Pictures';
+
+
+
+
+
+var_dump($films);
+var_dump($cartoons);
